c34021c23096eb441f9b4627420cf72cfbc83f4a
Katie Seggelke <katie.seggelke@gmail.com>
2019-08-29 20:30:38 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -27,8 +27,11 @@ class Song
   
   def self.genre_count
     @@genres.each do |genre|
-      if @@genre_count.key? (genre)
-        
+      if @@genre_count.key?(genre)
+        @@genre_count[genre] += 1 
+      else 
+        @@genre_count[genre] = 1 
+      end 
     end 
   end 
   
